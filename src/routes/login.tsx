fe0fff788d288f62fab0b189e0c3514ea70727b7
Budi Gunawan <budiindra.g@gmail.com>
2024-09-08 15:02:48 +0700

    fix: add validation on login form

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import { ActionFunctionArgs, Form, Link, redirect } from "react-router-dom";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
+import {
+  ActionFunctionArgs,
+  Link,
+  redirect,
+  useSubmit,
+  Form,
+  useNavigation,
+} from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { authCookie } from "../lib/auth";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type LoginResponse = {
   message: string;
@@ -12,11 +22,12 @@ type LoginResponse = {
     token: string;
   };
 };
+
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
 
   const userData = {
-    username: formData.get("userName"),
+    username: formData.get("username"),
     password: formData.get("password"),
   };
 
@@ -44,25 +55,63 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   }
 };
 
+const formSchema = z.object({
+  username: z.string().min(5),
+  password: z.string().min(5),
+});
+
 export function LoginRoute() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const { state } = useNavigation();
+
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+
+  const submit = useSubmit();
+
+  const onSubmit = (userData: z.infer<typeof formSchema>) => {
+    submit(userData, {
+      method: "post",
+    });
+  };
+
   return (
     <div className="flex items-center gap-4">
       <div className="w-1/2">
         <h1 className="text-4xl font-poppins font-bold mb-4">Masuk ke Akun</h1>
-        <Form method="POST" className="w-full flex flex-col gap-4">
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          method="POST"
+          className="w-full flex flex-col gap-4"
+        >
           <div>
             <label
-              htmlFor="userName"
+              htmlFor="username"
               className="block text-sm font-medium leading-6 text-gray-900 mb-2"
             >
               Username
             </label>
-            <Input id="userName" name="userName" type="text" required />
+            <Input
+              id="username"
+              type="text"
+              {...register("username")}
+              required
+            />
+            {errors.username && (
+              <span className="text-sm text-red-500">
+                {errors.username?.message}
+              </span>
+            )}
           </div>
           <div>
             <label
@@ -74,7 +123,7 @@ export function LoginRoute() {
             <div className="relative">
               <Input
                 id="password"
-                name="password"
+                {...register("password")}
                 type={showPassword ? "text" : "password"} // Toggle between 'text' and 'password'
                 required
                 className="pr-10" // Adjust padding to make room for the icon
@@ -91,11 +140,17 @@ export function LoginRoute() {
                 )}
               </button>
             </div>
+            {errors.password && (
+              <span className="text-sm text-red-500">
+                {errors.password?.message}
+              </span>
+            )}
           </div>
           <div className="flex gap-5 mt-8">
             <Button
               className="w-full bg-j-green-dark hover:bg-j-green-darker"
               type="submit"
+              disabled={state === "submitting" || state === "loading"}
             >
               Masuk
             </Button>
