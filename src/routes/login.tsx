76bfa745e78d0a419df270379d20da8ee08a762c
Budi Gunawan <budiindra.g@gmail.com>
2024-09-08 16:31:12 +0700

    feat: add validation on login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -56,8 +56,8 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 const formSchema = z.object({
-  username: z.string().min(5),
-  password: z.string().min(5),
+  username: z.string().min(3),
+  password: z.string().min(6),
 });
 
 export function LoginRoute() {
