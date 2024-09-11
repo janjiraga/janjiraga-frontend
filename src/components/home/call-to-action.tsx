5c9128f89154377dd3135141ccad7675472c90d3
Budi Gunawan <budiindra.g@gmail.com>
2024-09-11 14:45:22 +0700

    feat: add responsive styling on homepage

--- src/components/home/call-to-action.tsx
+++ src/components/home/call-to-action.tsx
@@ -3,14 +3,14 @@ import { Button } from "../ui/button";
 
 export default function CallToAction() {
   return (
-    <div className="flex items-center bg-j-green-lighter mt-28 mb-52 p-10 rounded-2xl">
-      <div className="w-1/2 ">
+    <div className="flex flex-col lg:flex-row items-center bg-j-green-lighter mt-28 mb-52 p-10 md:rounded-2xl">
+      <div className="w-full">
         <img
           src="https://ucarecdn.com/089a8bac-8176-4d7d-a419-cac858c35bd9/172DiversityInclusionRevision.png"
           alt="diversity people"
         />
       </div>
-      <div className="w-1/2 font-poppins">
+      <div className="font-poppins text-center lg:text-left">
         <h2 className="font-medium text-3xl text-j-gray-dark mb-10">
           Temukan kawan mainmu sekarang!
         </h2>
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,12 +3,12 @@ import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
-    <div className="flex items-center">
-      <div className="font-poppins">
+    <div className="flex flex-col lg:flex-row items-center mb-10 p-4 lg:p-0">
+      <div className="font-poppins order-2 lg:order-1 text-center lg:text-left">
         <h1 className="font-medium text-4xl text-j-gray-dark">
           Tempat dimana olahraga menjadi pemersatu
         </h1>
-        <p className="text-xl text-j-gray-light mt-12 mb-14 max-w-sm">
+        <p className="text-xl text-j-gray-light mt-12 mb-14 ">
           Bergabunglah dan temukan teman baru untuk aktivitas olahraga yang
           lebih menyenangkan.
         </p>
@@ -18,7 +18,7 @@ export default function Hero() {
           </Button>
         </Link>
       </div>
-      <div className="w-full">
+      <div className="w-full order-1 lg:order-2">
         <img
           src={
             "https://ucarecdn.com/d428de18-7ad4-4158-901a-a299f240a8f5/-/preview/"
