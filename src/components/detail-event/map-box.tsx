fca6f7f314445e494982c32f0d7110a3a81b55b2
Budi Gunawan <budiindra.g@gmail.com>
2024-09-11 13:43:12 +0700

    feat: add google map link

--- src/components/detail-event/map-box.tsx
+++ src/components/detail-event/map-box.tsx
@@ -26,19 +26,31 @@ export const MapBox = ({ venue }: MapBoxParams) => {
   }, [venue]);
 
   return (
-    <Map
-      {...viewState}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      style={{ width: "100%", height: "500px" }}
-      mapboxAccessToken={mapBoxToken}
-      initialViewState={viewState}
-      onMove={(event) => setViewState(event.viewState)}
-    >
-      <Marker
-        longitude={venue?.longitude}
-        latitude={venue?.latitude}
-        color="red"
-      />
-    </Map>
+    <>
+      <Map
+        {...viewState}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        style={{ width: "100%", height: "500px" }}
+        mapboxAccessToken={mapBoxToken}
+        initialViewState={viewState}
+        onMove={(event) => setViewState(event.viewState)}
+      >
+        <Marker
+          longitude={venue?.longitude}
+          latitude={venue?.latitude}
+          color="red"
+        />
+      </Map>
+      <div className="mt-4">
+        <a
+          className="text-j-green-dark hover:text-j-green-darker font-semibold"
+          href={venue.mapsUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Petunjuk Jalan
+        </a>
+      </div>
+    </>
   );
 };
