09ef6f8234aeff65e39a83b57c6b68f5feb65cf5
Budi Gunawan <budiindra.g@gmail.com>
2024-09-12 09:49:38 +0700

    feat: adjust styling on about page and delete unused image files

--- src/components/ui/shared/site-navigation.tsx
+++ src/components/ui/shared/site-navigation.tsx
@@ -14,18 +14,24 @@ export function SiteNavigation() {
     <div className="border-b border-b-gray-light fixed top-0 w-full z-10 bg-white">
       <nav className="py-4 flex justify-between items-center px-4 md:px-0 xl:max-w-6xl lg:max-w-4xl md:max-w-2xl mx-auto">
         <div className="flex items-center">
-          <img src={Logo} alt="logo janjiraga" className="h-10 w-10 mr-3" />
+          <img src={Logo} alt="logo janjiraga" className="h-10 w-10" />
           <Link to="/" className="font-poppins text-lg md:text-2xl font-bold">
             Janjiraga
           </Link>
         </div>
 
-        <div className="ml-6 ">
+        <div className="ml-6 flex gap-3">
           <Link
             to="/events"
             className="font-poppins hidden md:inline text-gray-600"
           >
             Main Bareng
+          </Link>
+          <Link
+            to="/about"
+            className="font-poppins hidden md:inline text-gray-600"
+          >
+            About
           </Link>
         </div>
 
